7abd618f24eaf9509fb7cbf4e07c5a50b113f2ba
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-12-11 08:31:10 +0530

    Added : Adding Details

---
+++ src/main/java/com/bridgelabz/LoginServlet.java
@@ -0,0 +1,41 @@
+package com.bridgelabz;
+        import javax.servlet.RequestDispatcher;
+        import javax.servlet.ServletException;
+        import javax.servlet.annotation.WebInitParam;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
+        import java.io.PrintWriter;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = {"/LoginServlet"},
+        initParams = {
+                @WebInitParam(name = "user", value = "shivani"),
+                @WebInitParam(name = "password", value = "1234")
+        }
+)
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // get request param for usr and password
+        String user=req.getParameter("SHIVANI");
+        String pwd=req.getParameter("Bridgelabz");
+        //get servlet config init params
+        String userID=getServletConfig().getInitParameter("user");
+        String password=getServletConfig().getInitParameter("password");
+        if (userID.equals(user) && password.equals(pwd)){
+            req.setAttribute("user",user);
+            req.getRequestDispatcher("LoginSuccess.jsp" ).forward(req,resp);
+
+        }
+        else {
+            RequestDispatcher rd=getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out= resp.getWriter();
+            out.println("<font color=red> Either User Name Or Password is Wrong </font> ");
+            rd.include(req,resp);
+        }
+    }
+}
